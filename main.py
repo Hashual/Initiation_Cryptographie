8f463acf65cb5b9082591b74ad500a44dd9d0df3
Hashual <loizeau.dorian05@gmail.com>
2024-10-15 14:21:13 +0200

    Add : new functions to menu

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from Hachage import *
 
 if __name__ == "__main__":
-    print("MENU :")
+    fichier_csv = "masterPassword.csv"
+    
+    mot_de_passe_maitre = lire_mdp_maitre_csv(fichier_csv)
+    if mot_de_passe_maitre is None:
+        print("Aucun mot de passe maître trouvé.")
+        mot_de_passe_maitre = demander_nouveau_mdp_maitre()
+        ecrire_mdp_maitre_csv(fichier_csv, mot_de_passe_maitre)
+
+    print("\nMENU :")
     print("1. Générer un mot de passe")
+    print("2. Changer le mot de passe maître")
+    print("3. Quitter")
     choice = input()
     if choice == "1":
         print("Entrez la première chaine de caractères : ")
@@ -21,6 +31,15 @@ if __name__ == "__main__":
             else :
                 break
         print("Le mot de passe généré est : ", hachagePrimitif(chain1, chain2, int(passwordLenght)))
-
-    else :
-        print("Choix invalide")
+    
+    elif choice == "2":
+            mot_de_passe_maitre = demander_nouveau_mdp_maitre()
+            ecrire_mdp_maitre_csv(fichier_csv, mot_de_passe_maitre)
+            print("Le mot de passe maître a été changé.")
+    
+    elif choice == "3":
+        print("Sortie...")
+        
+    
+    else:
+        print("Choix invalide.")
